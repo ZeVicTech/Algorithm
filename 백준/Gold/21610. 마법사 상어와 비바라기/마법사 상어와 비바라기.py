39fb293900e059a45df45952abff9419e886a020
ZeVicTech <113289544+ZeVicTech@users.noreply.github.com>
2024-04-07 02:34:40 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 280 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,49 +1,29 @@
 import sys
-from copy import deepcopy
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 water = [list(map(int, input().split())) for _ in range(n)]
-move = []
-cloud_map = [[0 for _ in range(n)] for _ in range(n)]
-cloud_map[n-1][0] = 1
-cloud_map[n-1][1] = 1
-cloud_map[n-2][0] = 1
-cloud_map[n-2][1] = 1
-cloud = [(n-1, 0), (n-1, 1), (n-2, 0), (n-2, 1)] # 초기 구름
-
-for _ in range(m):
-    move.append(tuple(map(int, input().split())))
+move = [list(map(int, input().split())) for _ in range(m)]
+cloud = [(n-1,0), (n-1,1), (n-2, 0), (n-2, 1)] # 초기 구름
 
 dx = [0, -1, -1, -1, 0, 1, 1, 1]
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 
+
 def move_cloud(direct, distance):
-    global cloud, cloud_map
-    cloud_map_temp = [[0 for _ in range(n)] for _ in range(n)]
-    cloud_temp = []
+    cloud_temp = set()
     for x, y in cloud:
         nx = (x + dx[direct] * distance) % n
         ny = (y + dy[direct] * distance) % n
 
-        if nx < 0:
-            nx = n + nx -1
-        if ny < 0:
-            ny = n + ny -1
-
-        # 이동한 위치에 구름 생성
-        cloud_map_temp[nx][ny] = 1
-        cloud_temp.append((nx,ny))
+        cloud_temp.add((nx,ny))
         water[nx][ny] += 1
-    cloud = cloud_temp
-    cloud_map = cloud_map_temp
+    return cloud_temp
 
-def increment_water():
+def copy_water():
     global water
     for x, y in cloud:
-        for i in range(8):
-            if i % 2 == 0:
-                continue
+        for i in range(1, 8, 2):
             nx = x + dx[i]
             ny = y + dy[i]
             if not(0 <= nx < n and 0 <= ny < n):
@@ -52,27 +32,23 @@ def increment_water():
                 water[x][y] += 1
 
 def create_cloud():
-    global cloud, cloud_map
-    cloud_map_temp = [[0 for _ in range(n)] for _ in range(n)]
-    cloud_temp = []
+    cloud_temp = set()
     for i in range(n):
         for j in range(n):
-            if cloud_map[i][j] == 1:
+            if (i,j) in cloud:
                 continue
             if water[i][j] < 2:
                 continue
-            cloud_map_temp[i][j] = 1
-            cloud_temp.append((i,j))
+            cloud_temp.add((i,j))
             water[i][j] -= 2
 
     # 새로운 구름 생성
-    cloud = cloud_temp
-    cloud_map = cloud_map_temp
+    return cloud_temp
 
 for direct, distance in move:
-    move_cloud(direct-1, distance)
-    increment_water()
-    create_cloud()
+    cloud = move_cloud(direct-1, distance)
+    copy_water()
+    cloud = create_cloud()
 
 result = 0
 for i in range(n):
