3310d7bb2f9332536895ec375d5f01dcb5da1b03
YeongSeung Kim <113289544+ZeVicTech@users.noreply.github.com>
2024-10-18 22:12:46 +0900

    [Silver II] Title: 주식, Time: 2676 ms, Memory: 170904 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -4,14 +4,15 @@ input = sys.stdin.readline
 t = int(input())
 
 for _ in range(t):
-    day = int(input())
-    stock = list(map(int, input().split()))
-    high = 0
-    total = 0
-    for i in range(day-1,-1,-1):
-        if high <= stock[i]:
-            high = stock[i]
-        else:
-            total += high - stock[i]
-    
-    print(total)
+    n = int(input())
+    stock_prices = list(map(int, input().rstrip().split()))
+
+    profit = 0
+    max_price = stock_prices[-1]
+    for price in stock_prices[::-1]:
+        if max_price < price:
+            max_price = price
+            continue
+        profit += max_price - price
+
+    print(profit)
